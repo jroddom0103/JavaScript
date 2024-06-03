abc5c6423f773797d9add45c12b0ea0ec226ea8c
jroddom0103 <jroddom0103@g.educaand.es>
2024-06-03 14:08:19 +0200

    Añado

---
+++ Proyecto1/logica.js
@@ -0,0 +1,8 @@
+console.log("Hola Mundo")
+    console.log("Mensaje adicional")
+
+    var parrafo = document.getElementById("pParrafo")
+    console.log(parrafo.textContent)
+
+    parrafo.textContent = "Otro texto muy diferente"
+    console.log(parrafo.textContent)
