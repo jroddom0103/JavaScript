ed621c9d479f6905c83ccd83909bc46b0d3ceffb
jroddom0103 <jroddom0103@g.educaand.es>
2024-06-03 14:44:56 +0200

    Añado

--- Proyecto1/logica.js
+++ Proyecto1/logica.js
@@ -9,19 +9,11 @@ console.log("Hola Mundo")
 
     var textoPrueba = "" //Declaramos una variable con nombre textoPrueba
     
-    textoPrueba = prompt("Escribe hola") //prompt devuelve un String, así que lo que hacemos es 
-    // almacenar ese String en la variable textoPrueba
-    parrafo.textContent = textoPrueba
-    if(parrafo.textContent=="Hola"){
-        console.log("Lo pusiste bien")
-    }else{
-        parrafo.textContent=""
-        console.log("Lo pusiste mal")
-    }
-    
     /************************/
 
-    var textoNombre = prompt("Pon el nombre")
-    var textoApellido1 = prompt("Pon el primer apellido")
-    var textoApellido2 = prompt("Pon el segundo apellido")
-    console.log(textoNombre+" "+textoApellido1+" "+textoApellido2)
+    console.log("1"+1)
+    console.log(1+"1")
+    console.log("1"-1)
+    console.log("a"-1)
+    console.log("Abra">"Pikachu")
+    console.log(true+"17")
